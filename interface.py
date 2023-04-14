27aef6d542089932ae74249d83e67e8b96365c86
Nat Standridge <standridgen@gmail.com>
2023-04-14 16:24:08 -0600

    Added validation loop, modified neural net, and added tqdm progress bars.

--- interface.py
+++ interface.py
@@ -2,28 +2,32 @@ import cv2
 from net import Net
 from loader import FER2013
 import torch
+import numpy as np
 
 def detect_and_draw(frame):
     ''' Processes the frame and returns it with a rectangle drawn around the detected face. '''
     haar = cv2.CascadeClassifier("assets/haarcascade_frontalface.xml")     ## use haar cascade to locate face
-
+    faces = haar.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 10, minSize = (75, 75))
+    
+    ## start preprocessing for passing to neural net
     gray_frame = cv2.resize(frame, (48, 48))
+    gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_BGR2GRAY)   ## convert to gray
+    dest = np.zeros((48, 48))
+    gray_frame = cv2.normalize(gray_frame, dest, 1.0, 0.0, cv2.NORM_MINMAX)
 
-    gray_frame = cv2.cvtColor(gray_frame, cv2.COLOR_BGR2GRAY)                   ## convert to gray
+    # net = Net(1, 7)
+    # net.load_state_dict(torch.load('model.pt'))
+    # net.eval()
 
-    net = Net()
-    net.load_state_dict(torch.load('model_2.pt'))
-    net.eval()
+    # pred = net(torch.from_numpy(gray_frame).unsqueeze(0).unsqueeze(0).float())
 
-    pred = net(torch.from_numpy(gray_frame).unsqueeze(0).unsqueeze(0).float())
+    ##pred = torch.argmax(pred).item()
 
-    pred = torch.argmax(pred).item()
-
-    faces = haar.detectMultiScale(gray_frame, scaleFactor = 1.1, minNeighbors = 10, minSize = (75, 75))
+    print(faces)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 20, 20), 3)
-    return(frame, pred)
+    return(frame)
 
 def main():
     capture = cv2.VideoCapture(0)
@@ -34,10 +38,10 @@ def main():
         if not val:
             break
 
-        frame, pred = detect_and_draw(frame) ## get the frame with rectangle drawn around the face
+        new_frame = detect_and_draw(frame) ## get the frame with rectangle drawn around the face
 
-        cv2.imshow('Live Emotion Detector', frame)
-        print(f"Emotion prediction is: {pred}")
+        cv2.imshow('Live Emotion Detector', new_frame)
+        ##print(f"Emotion prediction is: {pred}")
 
         if cv2.waitKey(1) & 0xFF == ord('c'):
             break
--- net.py
+++ net.py
@@ -1,39 +1,69 @@
 import torch
-from torch import nn, save
+from torch import nn, save, flatten
+from torch.nn import LogSoftmax
 from torch.optim import Adam
-from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader, random_split
+from torchvision.transforms import transforms
+from tqdm import tqdm
+
 from loader import FER2013
 
-train = FER2013(train = True, transform = ToTensor())
-dataset = DataLoader(train, batch_size = 32, shuffle = True, pin_memory = True, num_workers = 16)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,),(0.5,))
+])
+
+FER2013_data = FER2013(train = True, transform = transform) ## turn to tensor and normalize pixel values
+train_ratio = 0.8
+
+num_training_samples = int(len(FER2013_data) * train_ratio)
+num_val_samples = len(FER2013_data) - num_training_samples
+train_dataset, val_dataset = random_split(FER2013_data, [num_training_samples, num_val_samples])
+
+train_loader = DataLoader(train_dataset, batch_size = 256, shuffle = True, pin_memory = True, num_workers = 0)  ## have to set num_workers 0 now for some reason
+val_loader = DataLoader(val_dataset, batch_size = 256, shuffle = False, pin_memory = True, num_workers = 0)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_channels, classes):
         super().__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 32, (5, 5), padding = 1), ## one input channel (because its grayscale)
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, (5, 5), padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 64, (5, 5), padding = 1),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Flatten(),
-            nn.Linear(4096, 128),
-            nn.ReLU(),
-            nn.Linear(128, 7)
-        )
+        self.conv1 = nn.Conv2d(num_channels, 32, (5, 5), padding = 2)
+        nn.init.kaiming_uniform_(self.conv1.weight, mode='fan_in', nonlinearity='relu')
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.conv2 = nn.Conv2d(32, 128, (5, 5), padding = 2)
+        nn.init.kaiming_uniform_(self.conv2.weight, mode='fan_in', nonlinearity='relu')
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.conv3 = nn.Conv2d(128, 128, (3, 3), padding = 1)
+        self.relu3 = nn.ReLU()
+        ##self.dropout = nn.Dropout(0.3)
+        self.fc1 = nn.Linear(18432, 64)
+        nn.init.kaiming_uniform_(self.conv1.weight, mode='fan_in', nonlinearity='relu')
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(64, classes)
+        self.logSM = LogSoftmax(dim=1)
 
     def forward(self, x):
-        return(self.model(x))
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu4(x)
+        x = self.fc2(x)
 
-def train(epochs = 50):
-    n = Net()
+        return(self.logSM(x))
+
+def train(epochs = 3):
+    n = Net(1, 7) ## one input channel (grayscale), 7 classes (emotions)
     n = n.to(device)
+    training_loss = 0.0
 
     ''' 
     Adam is a stochastic gradient descent optimizer that implements adaptive learning rates.
@@ -42,7 +72,7 @@ def train(epochs = 50):
     Adam takes the neural net parameters, and a learning rate.
     (higher learning rate causes parameters to be updated more aggressively)
     '''
-    optimize = Adam(n.parameters(), lr = 0.01)
+    optimize = Adam(n.parameters(), lr = 0.0001)
 
     '''
     A loss function calculates how well the model is performing (predictions vs correct labels)
@@ -50,21 +80,42 @@ def train(epochs = 50):
     '''
     loss_function = nn.CrossEntropyLoss()
 
-    for epoch in range(epochs):
-        print(f"Starting epoch {epoch}")
-        for index, batch in enumerate(dataset):
-            print(f"Starting batch {index}")
-            data, label = batch
-            data, label = data.to(device), label.to(device)
-            yhat = n(data)
-            loss = loss_function(yhat, label)
+    for epoch in tqdm(range(epochs)):
+        for data, label in tqdm(train_loader):
+            data, label = data.to(device), label
+            output = n(data).to(device)
+            loss = loss_function(output, label)
 
-            ## back propogation begins
+            ## back propogation starts here
             optimize.zero_grad()
             loss.backward()
             optimize.step()
 
-        print(f"Epoch: {epoch}, loss: {loss.item()}")
+            training_loss += loss.item()
+
+        ## start validation
+        n.eval()
+        val_loss = 0.0
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for data, label in tqdm(val_loader):
+                data, label = data.to(device), label
+                output = n(data).to(device)
+                loss = loss_function(output, label)
+                val_loss += loss.item()
+
+                _, predicted = torch.max(output.data, 1)        ## ignore max value, get predicted label to compare to ground truth
+                total += label.size(0)                          ## get number of samples in current batch and add to total
+
+                correct += (predicted == label).sum().item()    ## compare predicted to ground truth, .sum() to count all True bools, .item() converts from single element tensor to a Python int/float 
+
+        train_loss /= len(train_loader)         ## average training loss
+        val_loss /= len(val_loader)             ## average validation loss
+        val_accuracy = 100 * correct / total    ## percentage of correctly identified label instances
+
+        print(f"Epoch: {epoch + 1} - Training loss: {training_loss:.5f} - Validation loss: {val_loss:.5f} - Validation accuracy: {val_accuracy:.5f}") ## add 1 to epoch when displaying to avoid counting from 0
 
     with open('model.pt', 'wb') as f:
         save(n.state_dict(), f)
